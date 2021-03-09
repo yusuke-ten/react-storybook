03f7c39bc8ae73ff418df480a438b649665e45fe
yusuke-ten <net.escape.com@gmail.com>
2021-03-10 00:33:54 +0900

    start storybook tutorial

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,9 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  //👇 Location of our stories
+  stories: ['../src/components/**/*.stories.tsx'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
-}
+};
